9dc9b5efce1d25bfc4a8b97158461887e7679f80
Dhagemann7 <36168264+Dhagemann7@users.noreply.github.com>
2021-04-13 13:57:49 -0400

    Update testArea.py

--- Main/testArea.py
+++ Main/testArea.py
@@ -10,7 +10,6 @@ def rgb_to_hex(rgb):
 
 #https://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
-print(rgb_to_hex((0,0,0)))
 lib_path = dir_path[:-4] + 'lib'
 #https://medium.com/the-owl/converting-mnist-data-in-idx-format-to-python-numpy-array-5cb9126f99f1
 filename = {'images' : (lib_path + '\\train-images.idx3-ubyte'), 'labels' : (lib_path + '\\train-labels.idx1-ubyte')}
@@ -33,6 +32,6 @@ if(first):
     for x in range(len(images_array[0])):
         for y in range(len(images_array[0][0])):
             tkinter.Label(top, bg =  '#'+rgb_to_hex((images_array[0][x][y], images_array[0][x][y], images_array[0][x][y])),
-            bd = 0, padx = 0, pady = 0, width = 1, height = 1, borderwidth=0 ).grid(row=x,column=y)
+            bd = 0, padx = 0, pady = 0, width = 1 px, height = 1 px, borderwidth=0 ).grid(row=x,column=y)
 first = False
 top.mainloop()
